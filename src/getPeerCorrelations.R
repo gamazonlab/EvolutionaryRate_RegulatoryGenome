499b8b816d2a4511be6d7e79b1129cc54551e615
gamazonlab <57782726+gamazonlab@users.noreply.github.com>
2020-05-31 12:38:51 -0500

    Add files via upload

--- src/getPeerCorrelations.R
+++ src/getPeerCorrelations.R
@@ -1,11 +1,16 @@
-getPeerCorrelation <- function(evofile, peerfle, outfile) {
+getPeerCorrelation <- function(evofile, peerfile, outfile) {
     evodata <- read.table(evofile, header = TRUE, sep = "\t")
     peerdata <- read.table(peerfile, header = TRUE, sep = "\t", row.names = 1)
     proteins <- evodata[which(evodata$Type == "protein_coding"),]
-    sortprotein <- proteins[order(proteins$GeneID),]
+    proteins <- proteins[order(proteins$GeneID),]
     proteins <- proteins[,c(1,14:19)]
     peerdata <- peerdata[order(rownames(peerdata)),]
     peerdata <- peerdata[which(rownames(peerdata) %in% proteins[,1]),]
-    proteins <- evodata[which(proteins[,1] %in% rownames(peerdata)),]
-    
+    proteins <- proteins[which(proteins[,1] %in% rownames(peerdata)),]
+    sdpeer <- apply(peerdata,1,sd)
+    sdcor <- cor.test(proteins$Mouse_dN.dS,sdpeer, method = c("spearman"))
+    outmat <- matrix(nrow=1, ncol=2)
+    outmat[1,1] <- peerfile
+    outmat[1,2] <- sdcor$estimate
+    write.table(outmat,file=outfile, quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
   }
